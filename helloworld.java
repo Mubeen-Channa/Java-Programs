2beeec57e55004b5edc5bb18ed2777d4e0c635dc
Muhammad Mubeen <116976135+Mubeen-Channa@users.noreply.github.com>
2023-05-03 00:22:11 +0500

    Add files via upload
    Simple Java Frist Hello World! Program

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+public class helloworld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.println();
+
+        System.out.println("        Information");
+        System.out.println("Name        : Muhammad Mubeen");
+        System.out.println("Caste       : Channa");
+        System.out.println("From        : Larkana");
+        System.out.println("Profession  : Software Engineer");
+    }
+}
