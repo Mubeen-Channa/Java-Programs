7663ad8a782b32b54ba5c0f1dd02772a78a2f106
Muhammad Mubeen Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2024-04-08 13:35:59 +0500

    Day 3 Leaning of C++

---
+++ _13_User_Input.cpp
@@ -0,0 +1,19 @@
+// Day 2
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    /*
+        cout is used to output print values 
+        and cin to get user input
+
+        cin is a predefined variable that reads data from the keyboard with the extraction operator >>.
+
+        for example:
+    */
+        string name; 
+        cout << "Type your name : ";        // Type your name and press enter
+        cin >> name;                        // Get user input from the keyboard
+        cout << "Your name is : " << name;  // Display the input value   
+}
