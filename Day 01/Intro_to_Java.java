6fe672c1e990db82bb939ad2f986bf2324c8d24f
Muhammad Mubeen Channa <muhammadmubeen.bscskdkf22@iba-suk.edu.pk>
2024-05-18 16:30:55 +0500

    Day 01 Learning of Java

---
+++ Day 01/Intro_to_Java.java
@@ -0,0 +1,20 @@
+// JAVA
+/*  
+    Java is a popular programming language, created in 1995.
+    Java is a high-level, object-oriented programming language
+    Java is used to develop mobile apps, web apps, desktop apps, games and much more. 
+    
+    To Run a Java "JDK" (Java Development Kit) is nessesary to intall.
+    Java's "write once, run anywhere" capability allows compiled code to run on any platform with a Java Virtual Machine (JVM).
+*/
+
+
+// Syntax
+/*
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Mubeen Channa");    // It is used to print a value to the screen.
+        }
+    }
+
+*/
